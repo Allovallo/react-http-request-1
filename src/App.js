49ba49b1263c62a09c9c6ca1686a0818d57633b1
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-05-30 18:04:34 +0300

    files updated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import { Component } from 'react';
-import { ToastContainer } from 'react-toastify';
 
 export default class App extends Component {
+  state = {
+    pokemon: null,
+  };
+
   componentDidMount() {
     fetch('http://pokeapi.co/api/v2/pokemon/ditto')
       .then(response => response.json())
-      .then(console.log);
+      .then(pokemon => this.setState({ pokemon }));
   }
 
   render() {
     return (
       <div style={{ maxWidth: 1170, margin: '0 auto', padding: 20 }}>
-        <ToastContainer autoClose={3000} />
+        {this.state.pokemon && <div> Тут буде покемон після fitch'у та коли в state запишемо</div>}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import 'modern-normalize/modern-normalize.css';
+import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
