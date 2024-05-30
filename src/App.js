19f70de98eef94b62c00c2333043a5905ca04718
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-05-30 15:54:24 +0300

    files uploaded

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { ToastContainer } from 'react-toastify';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div style={{ maxWidth: 1170, margin: '0 auto', padding: 20 }}>
+        <ToastContainer autoClose={3000} />
+      </div>
+    );
+  }
+}
