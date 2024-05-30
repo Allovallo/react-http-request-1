1bddf641580c1171316f81775267fa20567ccd05
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-05-30 17:19:15 +0300

    files updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import { ToastContainer } from 'react-toastify';
 
 export default class App extends Component {
+  componentDidMount() {
+    fetch('http://pokeapi.co/api/v2/pokemon/ditto')
+      .then(response => response.json())
+      .then(console.log);
+  }
+
   render() {
     return (
       <div style={{ maxWidth: 1170, margin: '0 auto', padding: 20 }}>
