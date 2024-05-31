e0de2b5b8450fe8422af418ceaa181fd12cd255a
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-05-31 10:31:06 +0300

    App updated

--- src/App.js
+++ src/App.js
@@ -3,18 +3,23 @@ import { Component } from 'react';
 export default class App extends Component {
   state = {
     pokemon: null,
+    loading: false,
   };
 
   componentDidMount() {
+    this.setState({ loading: true });
+
     fetch('http://pokeapi.co/api/v2/pokemon/ditto')
       .then(response => response.json())
-      .then(pokemon => this.setState({ pokemon }));
+      .then(pokemon => this.setState({ pokemon }))
+      .finally(() => this.setState({ loading: false }));
   }
 
   render() {
     return (
       <div style={{ maxWidth: 1170, margin: '0 auto', padding: 20 }}>
-        {this.state.pokemon && <div> Тут буде покемон після fitch'у та коли в state запишемо</div>}
+        {this.state.loading && <h1>Завантажуємо...</h1>}
+        {this.state.pokemon && <div>{this.state.pokemon.name}</div>}
       </div>
     );
   }
