a1ac8df310a1018fb1b58eb53ae19871a3db310a
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-05-31 15:43:19 +0300

    files updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import PokemonForm from 'components/PokemonForm';
 import { ToastContainer } from 'react-toastify';
+import PokemonInfo from 'components/PokemonInfo';
 
 export default class App extends Component {
   state = {
@@ -15,6 +16,7 @@ export default class App extends Component {
     return (
       <div style={{ maxWidth: 1170, margin: '0 auto', padding: 20 }}>
         <PokemonForm onSubmit={this.handleFormSubmit} />
+        <PokemonInfo pokemonName={this.state.pokemonName} />
         <ToastContainer autoClose={3000} />
       </div>
     );
--- src/components/Pokemoninfo.js
+++ src/components/Pokemoninfo.js
@@ -1,7 +1,49 @@
 import React, { Component } from 'react';
 
 export default class PokemonInfo extends Component {
-  return() {
-    return <div>PokemonInfo</div>;
+  state = {
+    pokemon: null,
+    loading: false,
+    error: null,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const prevName = prevProps.pokemonName;
+    const nextName = this.props.pokemonName;
+
+    if (prevName !== nextName) {
+      console.log('Змінилося ім`я покемона!');
+
+      this.setState({ loading: true });
+
+      fetch(`https://pokeapi.co/api/v2/pokemon/${nextName}`)
+        .then(response => response.json())
+        .then(pokemon => this.setState({ pokemon }))
+        .catch(error => this.setState({ error }))
+        .finally(() => this.setState({ loading: false }));
+    }
+  }
+
+  render() {
+    const { pokemon, loading, error } = this.state;
+    const { pokemonName } = this.props;
+
+    return (
+      <div>
+        {error && <h1>Помилка! Поенмона з ім'ям {pokemonName} немає!</h1>}
+        {loading && <div>Завантажуємо...</div>}
+        {!pokemonName && <div>Введіть ім`я покемона!</div>}
+        {pokemon && (
+          <div>
+            <p>{pokemon.name}</p>
+            <img
+              src={pokemon.sprites.other['official-artwork'].front_default}
+              width="240"
+              alt="pokemon-name"
+            />
+          </div>
+        )}
+      </div>
+    );
   }
 }
