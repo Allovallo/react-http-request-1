392a0e16b3eddc7505a1cb01e309098a7659e25e
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-06-01 17:26:14 +0300

    files updated

--- src/components/Pokemoninfo.js
+++ src/components/Pokemoninfo.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PockemonErrorView from './PokemonErrorView';
 import PockemonDataView from './PokemonDataView';
 import PockemonPendingView from './PokemonPendingView';
+import pokemonAPI from '../services/pokemon-api';
 
 export default class PokemonInfo extends Component {
   state = {
@@ -17,17 +18,10 @@ export default class PokemonInfo extends Component {
     if (prevName !== nextName) {
       this.setState({ status: 'pending' });
 
-      setTimeout(() => {
-        fetch(`https://pokeapi.co/api/v2/pokemon/${nextName}`)
-          .then(response => {
-            if (response.ok) {
-              return response.json();
-            }
-            return Promise.reject(new Error(`Помилка! Покемону з ім'ям ${nextName} немає!`));
-          })
-          .then(pokemon => this.setState({ pokemon, status: 'resolved' }))
-          .catch(error => this.setState({ error, status: 'rejected' }));
-      }, 3000);
+      pokemonAPI
+        .fetchPokemon(nextName)
+        .then(pokemon => this.setState({ pokemon, status: 'resolved' }))
+        .catch(error => this.setState({ error, status: 'rejected' }));
     }
   }
 
